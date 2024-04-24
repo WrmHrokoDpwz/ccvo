c9987372c6b864b18eef7f56bee468e5956277a4
saif <167532762+WrmHrokoDpwz@users.noreply.github.com>
2024-04-25 00:24:16 +0700

    Add files via upload

---
+++ FallenMusic/Helpers/dossier.py
@@ -0,0 +1,99 @@
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from FallenMusic import BOT_NAME
+
+PM_START_TEXT = """
+‌‌‏≪⊶⌯━‌‌‏♢ ⦓ سورس جافا ⦔ ♢━‌‌‏⌯⊷≫
+
+⎊ اهــلا بـك » {0}
+
+⎊ انـا بــوت » {1}
+
+⎊ اســتـطـيـع تـشـغـيـل الـقـرآن الـكـريـم
+
+⎊ فــي الــــــمــــكـــالــمــات الــصــوتــيـــة
+
+⎊ تـابـع الازرار بـالاسفل لـمعرفة الاوامـࢪ
+
+‌‌‏≪⊶⌯━‌‌‏♢ ⦓ سورس جافا ⦔ ♢━‌‌‏⌯⊷≫
+"""
+
+START_TEXT = """
+━‌‌‏─━─『جافا』─━─━‌‌‏
+⎊ مـرحـبـا » {0} 
+⎊ انا بـوت استطيع تـشـغيـل
+⎊الـقـرآن الـكـريـم في الـكـول
+⎊ لـمـعـرفة اوامـر الـتـشـغـيـل
+⎊ ادخـــــــل الـــــى البــــــوت
+━‌‌‏─━─『جافا』─━─━‌‌‏
+"""
+
+HELP_TEXT = f"""
+<u>⎊ **الاوامر في بوت سورس جافا {BOT_NAME} :**</u>
+
+⎊ لـتـشـغـيل سـوره في الـمكـالـمة الـصـوتـيـة
+⎊ اكتب تشغيل + اسم السوره
+
+⎊ لإيقاف التشغيل في الكول مؤقتاً
+⎊ اكتب كتم او اسكت
+
+⎊ لأسـتـئـنـاف الـتـشغـيل في الـكـول
+⎊ اكتب كمل او الغاء الكتم
+
+⎊ للتخطي وتـشغـيل السـوره الـتالـية
+⎊ اكتب تخطي او التالي
+ 
+⎊ لإيـقـاف الـتـشـغـيـل فـي الـكـول
+⎊ اكتب ايقاف او قف
+
+⎊ اكتب بنج لمعرفة سرعة تشغيل البوت
+⎊ اكتب المطورين لمعرفة مطورين البوت
+⎊ اكتب تحميل + الاسم للتنزيل من اليوتيوب
+⎊ اكتب بحث + الاسم للبحث عن اغنية
+"""
+
+HELP_SUDO = f"""
+<u>⎊ **اوامر المطور {BOT_NAME} :**</u>
+
+⎊ اكتب المكالمات لمعرفة المكالمات في البوت
+⎊ اكتب /eval لتشغيل على محطة البوت 
+⎊ اكتب فحص لاختبار سرعة السيرفر  
+⎊ اكتب الاحصائيات لعرض إحصائيات السيرفر 
+
+⎊ اكتب /اسم + الاسم لوضعة اسم للمساعد
+⎊ اكتب /بايو + البايو لوضعة بايو للمساعد
+⎊ اكتب /صورة واعمل ريب على صورة للمساعد
+⎊ اكتب /مسح لحذف صورة الحساب المساعد 
+"""
+
+HELP_DEV = f"""
+<u>⎊ **اوامر المالك {BOT_NAME} :**</u>
+
+⎊ اكتب الفارات لجلب فارات تنصيب البوت
+⎊ اكتب اذاعة و اعمل ريب على الاذاعه
+⎊ اكتب تنظيف لمسح التخزين المؤقت للبوت
+⎊ اكتب مغادرة لمغادرة المساعد للجروبات
+
+⎊ اكتب /مطور +اليوزر لإضافة مطور في البوت
+⎊ اكتب /تنزيل +اليوزر لـتـنزيل مطور من البوت
+"""
