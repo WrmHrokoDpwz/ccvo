edec7290f327227d3804d6b7cb5a271c2d14b1ff
saif <167532762+WrmHrokoDpwz@users.noreply.github.com>
2024-04-25 01:06:58 +0700

    Update ping.py
    تحديث

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -60,7 +60,7 @@ async def ping_fallen(_, message: Message):
                     InlineKeyboardButton("الدعم", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
                         "السوࢪس",
-                        url="https://t.me/BV_YQ",
+                        url="https://t.me/WOFKC",
                     ),
                 ],
             ]
